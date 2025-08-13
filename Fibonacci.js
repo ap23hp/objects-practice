c2087b12fded7c33de04b6df0af1a419fe0fce81
Apra <apra.khanna2304@gmail.com>
2025-08-14 00:20:48 +0530

    getting the age of oldestperson from object and returning object

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,14 +1,13 @@
-
 // create a function that returns a specific member of the Fibonacci sequence:
 
 // A series of numbers in which each number ( Fibonacci number ) is the sum of the two preceding numbers. In this
-//  exercise, the Fibonacci sequence used is 1, 1, 2, 3, 5, 8, etc. 
+//  exercise, the Fibonacci sequence used is 1, 1, 2, 3, 5, 8, etc.
 // To learn more about Fibonacci sequences, go to: https://en.wikipedia.org/wiki/Fibonacci_sequence
 
 // fibonacci(4); // returns the 4th member of the series: 3  (1, 1, 2, 3)
 // fibonacci(6); // returns 8
 
-const fibonacci = function(num) {
+const fibonacci = function (num) {
   // Convert string numbers to actual numbers
   num = Number(num);
 
@@ -32,18 +31,50 @@ const fibonacci = function(num) {
 
 const books = [
   {
-    title: 'Book',
-    author: 'Name'
+    title: "Book",
+    author: "Name",
   },
   {
-    title: 'Book2',
-    author: 'Name2'
-  }
-]
+    title: "Book2",
+    author: "Name2",
+  },
+];
 //Your job is to write a function that takes the array and returns an array of titles:
-function getTheTitles(arr){
-  return arr.map(function(ele){
-return ele.title
-    })
+function getTheTitles(arr) {
+  return arr.map(function (ele) {
+    return ele.title;
+  });
 }
-console.log(getTheTitles(books)) // ['Book','Book2']
+console.log(getTheTitles(books)); // ['Book','Book2']
+
+//Given an array of objects representing people with a birth and death year, return the oldest person.
+//You should return the whole person object,
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+const findTheOldest = function (arr) {
+const ages= arr.map((ele)=> ele.yearOfDeath ? ele.yearOfDeath-ele.yearOfBirth : new Date().getFullYear()-ele.yearOfBirth)
+console.log(ages)
+let maxAge= ages.reduce((acc,curr)=>acc<curr ? acc=curr : acc,0)
+let oldestageindex= ages.indexOf(maxAge)
+return arr[oldestageindex]
+
+
+};
+
+
+console.log(findTheOldest(people))
