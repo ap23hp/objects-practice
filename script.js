7ad7b2b723999d3d012c228a9e4b243ee6c62b7c
Apra <apra.khanna2304@gmail.com>
2025-08-13 19:48:25 +0530

    map to names and map to objects

---
+++ script.js
@@ -0,0 +1,32 @@
+
+//You have an array of user objects, each one has user.name. Write the code that converts it into an array of names.
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 28 };
+
+let users = [ john, pete, mary ];
+
+let names=users.map((user)=>user.name)
+console.log(names)
+
+
+//You have an array of user objects, each one has name, surname and id.
+
+//Write the code to create another array from it, of objects with id and fullName, 
+// where fullName is generated from name and surname.
+
+
+let john1 = { name: "John", surname: "Smith", id: 1 };
+let pete1 = { name: "Pete", surname: "Hunt", id: 2 };
+let mary1 = { name: "Mary", surname: "Key", id: 3 };
+
+let users1 = [ john1, pete1, mary1 ];
+
+let usersMapped= users1.map(function(user1){
+return {fullName : `${user1.name} ${user1.surname}`,
+id : user1.id}
+})
+
+console.log(usersMapped)
+
