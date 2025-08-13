ee4db8b4665c63235d5c86b8c290f3316d974cc1
Apra <apra.khanna2304@gmail.com>
2025-08-13 20:41:53 +0530

    Create keyed object from array

--- script.js
+++ script.js
@@ -58,3 +58,47 @@ return arrr.reduce((acc,curr)=>acc+curr.age,0)/arrr.length
 }
 console.log(`-----------reduce-----------------------------`)
 console.log( getAverageAge(arr1))  // (25 + 30 + 29) / 3 = 28
+
+//Let’s say we received an array of users in the form {id:..., name:..., age:... }.
+
+//Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
+
+//For example:
+
+let users4 = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+
+function groupById(array) {
+  return array.reduce((obj, value) => {
+    obj[value.id] = value;
+    return obj;
+  }, {})
+}
+let usersById = groupById(users4);
+console.log(`------------users group by id-------------`)
+console.log(usersById)
+/*
+// after the call we should have:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
+
+//without reduce
+// function groupById(array) {
+//   let obj = {}; // start with an empty object
+
+//   for (let i = 0; i < array.length; i++) {
+//     let value = array[i];         // current element (object)
+//     obj[value.id] = value;        // add it to obj using id as the key
+//   }
+
+//   return obj; // return the final object
+// }
